d07d57f00f85c03df238e84403bf1ac574ae8975
Anoop Joe Cyriac <ajc@ajc-work.(none)>
2009-11-20 12:24:16 +0530

    syntax error in setup.py removed setuptools instead of distutils for creating egg

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
 		name='proto',
 		version='0.3.1',
-		packages=['proto']
+		packages=['proto'],
 		description = "The Proto! Python Async RPC based on ProtocolBuffers and TCP sockets.",
-		author = "Stanislav Yudin"
+		author = "Stanislav Yudin",
 		author_email = "decvar@gmail.com",
 		classifiers = ["Development Status :: Beta", "Framework :: ProtocolBuffers"],
 		install_requires=[
